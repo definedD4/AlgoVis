37be7fb02044428e807f351d36ac145af0fe2d2a
Дмитрий <definedd4@gmail.com>
2017-06-09 10:29:03 +0300

    Add CircleDisplay

---
+++ AlgoVis/AlgoVis.Presenation/CircleDispaly.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace AlgoVis.Presenation
+{
+    public class CircleDispaly : ReactiveObject
+    {
+        [Reactive] public object Content { get; set; }
+
+        [Reactive] public Color Background { get; set; } = Colors.Red;
+    }
+}
